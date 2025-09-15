7575d7668eea46f46aa6ea5d0e367c17e0198033
Flavio A Alvarenga <flavio.fgjj@live.com>
2025-09-15 15:58:42 -0300

    chore: vite build issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import tailwind from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser'
+  }
 })
